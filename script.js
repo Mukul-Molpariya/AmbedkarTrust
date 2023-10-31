c126eecd493d69ffbb90d50effcbb3b2dcbec304
Mukul <mukulmolpariya72@gmail.com>
2023-10-31 13:57:44 +0530

    correction done

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function opentab(tabname) {
 }
 
 
+function hide() {
+    if (col1.style.display != 'block') {
+        col1.style.display = 'block';
+        col2.style.display = 'none';
+        document.getElementById("btn").innerHTML = "हिंदी";
+    }
+    else {
+        col1.style.display = 'none';
+        col2.style.display = 'block';
+        document.getElementById("btn").innerHTML = "English";
+    }
+}
+
+function outOfService(){
+    window.location.href = 'main/temp.html';
+}
